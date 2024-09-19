b85b88e908d7c258dcc4e9c7fd473f55e46d555d
Arman Mohammadi <amoham42@Armans-MacBook-Pro-6.local>
2024-09-19 10:55:04 -0400

    fixed issues

--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -5,7 +5,7 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Staff = () => {
- 
+
   return (
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden w-screen`}
@@ -37,12 +37,12 @@ const Staff = () => {
           <div className="flex flex-col gap-2 p-16 pb-0">
             <h3 className="text-2xl font-medium">Professor</h3>
             <div className="frosted flex flex-col rounded-lg bg-violet-500">
-              <img className="aspect-square w-full rounded-t-lg object-contain" src="https://placehold.co/800x800" alt="Eric Ewing"></img>
+              <img className="aspect-square w-full rounded-t-lg object-contain" src="../Eric_Ewing.jpg" alt="Eric Ewing"></img>
               <div className="p-4">
                 <div className="text-md font-medium">Eric Ewing</div>
                 <div className="text-sm text-slate-400">(pronouns)</div>
                 <div className="mt-2 text-sm italic text-slate-600">
-                  ---
+                  THE GOAT HIMSELF.
                 </div>
               </div>
             </div>
@@ -50,14 +50,14 @@ const Staff = () => {
           <div className="flex flex-col gap-2 p-16 pb-0">
             <h3 className="text-2xl font-medium">Head TA</h3>
             <div className="frosted flex flex-col rounded-lg bg-violet-500">
-              <img className="aspect-square w-full rounded-t-lg object-contain" src="../../public/Narek.png" alt="Narek Harutyunyan"></img>
+              <img className="aspect-square w-full rounded-t-lg object-contain" src="../Narek.png" alt="Narek Harutyunyan"></img>
               <div className="p-4">
                 <div className="text-md font-medium">Narek Harutyunyan</div>
                 <div className="text-sm text-slate-400">he/him</div>
                 <div className="mt-2 text-sm text-white">
-                Hi! I’m a junior from Armenia studying computer engineering.
-                My favorite activity is ziplining (we have very high and long ones in Armenia). 
-                I’m so excited for CS1952z this semester.
+                  Hi! I’m a junior from Armenia studying computer engineering.
+                  My favorite activity is ziplining (we have very high and long ones in Armenia).
+                  I’m so excited for CS1952z this semester.
                 </div>
               </div>
             </div>
@@ -67,13 +67,13 @@ const Staff = () => {
           <div className="flex flex-col gap-2 p-16 pb-0">
             <h3 className="text-2xl font-medium">TA</h3>
             <div className="frosted flex flex-col rounded-lg bg-violet-500">
-              <img className="aspect-square w-full rounded-t-lg object-contain" src="../../public/Fuka.png" alt="Fuka Ikeda"></img>
+              <img className="aspect-square w-full rounded-t-lg object-contain" src="../Fuka.png" alt="Fuka Ikeda"></img>
               <div className="p-4">
                 <div className="text-md font-medium">Fuka Ikeda</div>
                 <div className="text-sm text-slate-400">she/her</div>
                 <div className="mt-2 text-sm italic text-white">
-                I’m a junior from Japan doing Computer Science and Design Engineering! &nbsp;
-                I love pets (both alive and robot… wait are robots alive?) so please bring your dog to lab (maybe not)
+                  I’m a junior from Japan doing Computer Science and Design Engineering! &nbsp;
+                  I love pets (both alive and robot… wait are robots alive?) so please bring your dog to lab (maybe not)
                 </div>
               </div>
             </div>
@@ -81,7 +81,7 @@ const Staff = () => {
           <div className="flex flex-col gap-2 p-16 pb-0">
             <h3 className="text-2xl font-medium">TA</h3>
             <div className="frosted flex flex-col rounded-lg bg-violet-500">
-              <img className="aspect-square w-full rounded-t-lg object-contain" src="../../public/Arman.png" alt="Arman Mohammadi"></img>
+              <img className="aspect-square w-full rounded-t-lg object-contain" src="../Arman.png" alt="Arman Mohammadi"></img>
               <div className="p-4">
                 <div className="text-md font-medium">Arman Mohammadi</div>
                 <div className="text-sm text-slate-400">he/him</div>
