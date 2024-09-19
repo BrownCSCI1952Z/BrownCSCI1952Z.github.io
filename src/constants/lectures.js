3f3290024bc3cf5a1739eae91f966640acc666b7
Eric <eric_ewing@brown.edu>
2024-09-19 14:05:46 -0400

    update lecture slides

--- src/constants/lectures.js
+++ src/constants/lectures.js
@@ -17,56 +17,57 @@ export const lectures = [
     {
       topic: "Robot State & Pose",
       date: "09/17",
-      slides: "https://docs.google.com/presentation/d/1nPe4iHonziLvBUCrI-XMC72RgbRXkmew3Jx0AJ_p7D8/edit#slide=id.g2b6d918aa3c_0_405",
-    },
-    {
-      topic: "Trajectories",
-      date: "09/19",
-      slides: "https://docs.google.com/presentation/d/1F4A1ctSRteDP1aNJr2wOmqYLQjxvLMFlRnyEzJiyvDg/edit#slide=id.g2b6d918aa3c_0_405",
-    },
-    {
-      topic: "Final Project Group Form and/or Configuration Spaces Topology",
-      date: "09/24",
-      slides: "https://docs.google.com/presentation/d/1iuOvmYFoPFBLvm7hFMDjzHfYbwm1V_dFU6BEiL8_f1M/edit#slide=id.p1",
-    },
-    {
-      topic: "Sensing & Project Overview",
-      date: "09/26",
-      slides: "https://docs.google.com/presentation/d/1chKqS8DxjsjHBxF6QyaRjhMsbUuo14Ke2AaXTwC-qTc/edit",
-    },
-    {
-      topic: "Guest Lecture?",
-      date: "10/01",
-      slides: "https://docs.google.com",
+      slides: "https://docs.google.com/presentation/d/1j0meTdLHQaFitEsQg5BKWsFWLEm0e-dcLiWQQ9eauvM/edit?usp=sharing",
     },
     {
       topic: "Feedback Control",
-      date: "10/03",
-      slides: "https://docs.google.com/presentation/d/1QZqrzEy8zP1hwieWtIQso4Y7REpcKNnl/edit#slide=id.p1",
+      date: "09/19",
+      slides: "https://docs.google.com/presentation/d/1j0meTdLHQaFitEsQg5BKWsFWLEm0e-dcLiWQQ9eauvM/edit?usp=sharing",
     },
-    {
-      topic: "Guest Lecture?",
-      date: "10/08",
-      slides: "https://docs.google.com",
-    },
-    {
-      topic: "Guest Lecture?",
-      date: "10/10",
-      slides: "https://docs.google.com",
-    },
-    {
-      topic: "Ground Robot Dynamics",
-      date: "10/15",
-      slides: "https://docs.google.com",
-    },
-    {
-      topic: "Quadcopter Dynamics",
-      date: "10/01",
-      slides: "https://docs.google.com",
-    },
-    {
-      topic: "Complex Sensors",
-      date: "10/01",
-      slides: "https://docs.google.com/presentation/d/1mbUbRkioDT1YRyd86eMAFqzps5y50yWQhmk7z8hd5m0/edit#slide=id.p1",
-    },
+    // {
+    //   topic: "Trajectories",
+    //   date: "09/19",
+    //   slides: "https://docs.google.com/presentation/d/1F4A1ctSRteDP1aNJr2wOmqYLQjxvLMFlRnyEzJiyvDg/edit#slide=id.g2b6d918aa3c_0_405",
+    // },
+    // {
+    //   topic: "Final Project Group Form and/or Configuration Spaces Topology",
+    //   date: "09/24",
+    //   slides: "https://docs.google.com/presentation/d/1iuOvmYFoPFBLvm7hFMDjzHfYbwm1V_dFU6BEiL8_f1M/edit#slide=id.p1",
+    // },
+    // {
+    //   topic: "Sensing & Project Overview",
+    //   date: "09/26",
+    //   slides: "https://docs.google.com/presentation/d/1chKqS8DxjsjHBxF6QyaRjhMsbUuo14Ke2AaXTwC-qTc/edit",
+    // },
+    // {
+    //   topic: "Guest Lecture?",
+    //   date: "10/01",
+    //   slides: "https://docs.google.com",
+    // },
+    
+    // {
+    //   topic: "Guest Lecture?",
+    //   date: "10/08",
+    //   slides: "https://docs.google.com",
+    // },
+    // {
+    //   topic: "Guest Lecture?",
+    //   date: "10/10",
+    //   slides: "https://docs.google.com",
+    // },
+    // {
+    //   topic: "Ground Robot Dynamics",
+    //   date: "10/15",
+    //   slides: "https://docs.google.com",
+    // },
+    // {
+    //   topic: "Quadcopter Dynamics",
+    //   date: "10/01",
+    //   slides: "https://docs.google.com",
+    // },
+    // {
+    //   topic: "Complex Sensors",
+    //   date: "10/01",
+    //   slides: "https://docs.google.com/presentation/d/1mbUbRkioDT1YRyd86eMAFqzps5y50yWQhmk7z8hd5m0/edit#slide=id.p1",
+    // },
 ];
